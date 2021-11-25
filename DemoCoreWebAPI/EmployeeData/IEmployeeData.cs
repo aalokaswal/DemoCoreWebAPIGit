be4ff7cbc90ff10d72c389d6389d3afebe6647d7
Alok.Aswal <Alok.Aswal@MI8>
2021-11-25 23:28:27 +0530

    Add project files.

---
+++ DemoCoreWebAPI/EmployeeData/IEmployeeData.cs
@@ -0,0 +1,22 @@
+﻿using DemoCoreWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoCoreWebAPI.EmployeeData
+{
+    public interface IEmployeeData
+    {
+        List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid id);
+
+        Employee AddEmployee(Employee employee);
+
+        void DeleteEmployee(Employee employee);
+
+        Employee EditEmployee(Employee employee);
+
+    }
+}
---
+++ DemoCoreWebAPI/EmployeeData/MockEmployeeData.cs
@@ -0,0 +1,42 @@
+﻿using DemoCoreWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoCoreWebAPI.EmployeeData
+{
+    public class MockEmployeeData : IEmployeeData
+    {
+        List<Employee> employees = new List<Employee>()
+        {
+            new Employee{Id = Guid.NewGuid(), Name="Alok"},
+            new Employee{Id = Guid.NewGuid(), Name="Aniket"}
+        };
+
+        public Employee AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee EditEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployee(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+    }
+}
